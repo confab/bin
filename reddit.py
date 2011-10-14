958b9fa0a8b9568bf5578620488f7f178d2d14f6
bunburya <bunburya@tcd.ie>
2011-10-15 00:20:14 +0100

    modified:   reddit.py

--- reddit.py
+++ reddit.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 from json import loads
@@ -15,9 +15,9 @@ def user(name):
     try:
         response = urlopen(url).readall().decode('utf-8')
     except HTTPError:
-        return (name, None, None)
+        return (None, None)
     data = loads(response)['data']
-    return (name, data['link_karma'], data['comment_karma'])
+    return (data['link_karma'], data['comment_karma'])
 
 def rand_item(sub='all'):
     result = []
@@ -25,7 +25,9 @@ def rand_item(sub='all'):
     try:
         data = loads(urlopen(url).read().decode('utf-8'))
     except ValueError:
-        return None
+        return None, None
+    if data.get('error', None) == '404':
+        return None, None
     posts = data['data']['children']
     post_data = choice(posts)['data']
     title = post_data['title']
@@ -37,9 +39,40 @@ def rand_item(sub='all'):
                 
 
 
-#def main():
-
-
+def main():
+    from sys import argv
+    if len(argv) < 2:
+        print('Syntax: reddit <command> <args>')
+        exit(1)
+    command = argv[1]
+    if len(argv) > 2:
+        args = argv[2:]
+    else:
+        args = None
+    if command == 'user':
+        if args is None:
+            print('Syntax: user <user1> <user2> ... <userN>')
+            exit(1)
+        for u in args:
+            link, comment = user(u)
+            if link is None:
+                response = 'User not found.'
+            else:
+                response = '{} link karma, {} comment karma.'.format(link, comment)
+            print('{}: {}'.format(u, response))
+    elif command == 'random':
+        if args is None:
+            args = ['all']
+        for s in args:
+            title, link = rand_item(s)
+            if title is None:
+                response = 'Subreddit not found.'
+            else:
+                response = '{} ({})'.format(title, link)
+            print('{}: {}'.format(s, response))
+    else:
+        print('Invalid command: {}. Valid commands are: user, random.'.format(command))
+            
 if __name__ == '__main__':
 	main()
 
